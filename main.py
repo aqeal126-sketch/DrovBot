1593147762d7d9dfe3f3028b0c67bb944f69fd37
aqeal126-sketch <aqeal126@gmail.com>
2026-05-30 17:15:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.exceptions import TelegramAPIError
-from aiohttp import web  # مكتبة الويب لخدعة البورت وبقاء البوت Online
+from aiohttp import web  # محرك الويب لضمان حالة Online المستمرة
 
 # --- الإعدادات الثابتة للمتجر ---
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -358,4 +358,4 @@ async def main_charge(call: types.CallbackQuery):
     await call.message.edit_text(text=s['charge_title'].format(balance=bal_disp), reply_markup=kb, parse_mode="Markdown")
 
 @dp.callback_query(F.data == "get_daily_gift")
-async def g
+async def get_dai
